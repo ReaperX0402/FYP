c1996dc492711e5aab73895b8e100bf1c915930c
Darren <darrenljy04@gmail.com>
2026-04-09 12:05:16 +0800

    Removal of docker system

--- src/app.py
+++ src/app.py
@@ -28,8 +28,10 @@ def create_app() -> Flask:
     init_session_factory(engine)
 
     # Init DB tables
+    """
     from src.db.init_db import init_db
     init_db(engine)
+    """
 
     # Register blueprints
     app.register_blueprint(auth_bp)
